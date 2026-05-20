Make masking fixes to ONNX modeling and add comparison tests with PyTorch model

betting_agent_to_onnx.py
@@ -3,14 +3,14 @@ import os
 
 from betting_env import CinchBettingEnv, BETS
 from get_agent import get_agent
-from obs_utils import build_betting_obs
+from obs_utils_torch import build_betting_obs
 
-BETTING_NET_CHECKPOINT_PATH = os.path.join("betting_net", "checkpoints_resnet-mlp-modified-agent", "ckpt_4900.pt")
+BETTING_NET_CHECKPOINT_PATH = os.path.join("checkpoints_resnet-mlp-modified-agent", "ckpt_4900.pt")
 DEVICE = "cpu"
 
 env = CinchBettingEnv()
 model = get_agent(env, BETTING_NET_CHECKPOINT_PATH)
-legal = env.legal_actions()
+# legal = env.legal_actions()
 obs = env._get_obs()
 x = build_betting_obs(obs).to(DEVICE)
 
@@ -20,9 +20,9 @@ x = build_betting_obs(obs).to(DEVICE)
 # 3. Export to an ONNX file
 torch.onnx.export(
     model, 
-    (x, [i * len(BETS) + l for l in legal for i in range(4)]),
+    (x),
     "betting_net.onnx",
-    input_names=["observation", "legal_actions"],
+    input_names=["observation"],
     output_names=["action_logits", "state_value"],
     opset_version=18
 )

get_onnx_agent.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import onnxruntime as ort
 
+from betting_env import BETS
+
 DEVICE = "cpu"
 
 class ONNXAgent:
@@ -39,8 +41,7 @@ def get_agent(env, onnx_path: str):
     return model
 
 
-def select_action(model, obs, env, obs_builder):
-    legal = env.legal_actions()
+def select_action(model, obs, legal, obs_builder, post_process_bet_logits=False):
     x = obs_builder(obs)
     logits, _ = model.predict(x)
 
@@ -48,7 +49,18 @@ def select_action(model, obs, env, obs_builder):
     masked = np.full_like(logits, -1e9)
     masked[legal] = logits[legal]
 
-    return int(np.argmax(masked))
+    # print("Model types:", type(model))
+    # print("Legal actions:", legal)
+    # print("Logits:", logits)
+    # print("Masked logits:", masked)
+    action = np.argmax(masked)
+    # print("Choice:", action)
+
+    if post_process_bet_logits:
+        # print("Post-processing bet logits:", (int(action % len(BETS)), int(action // len(BETS))))
+        return [int(action % len(BETS)), int(action // len(BETS))]
+
+    return [int(action)]
 
 if __name__ == "__main__":
     from main_env import CinchMainEnv, SUITS, RANKS
@@ -57,6 +69,7 @@ if __name__ == "__main__":
     env = CinchMainEnv()
 
     onnx_path = os.path.join(
+        "static",
         "main_net",
         "main_net.onnx"
     )
@@ -64,11 +77,12 @@ if __name__ == "__main__":
     model = get_agent(env, onnx_path)
 
     obs = env.reset()
+    legal = env.legal_actions()
 
     action = select_action(
         model=model,
         obs=obs,
-        env=env,
+        legal=legal,
         obs_builder=build_mlp_obs
     )
 

main_agent_to_onnx.py
@@ -3,9 +3,9 @@ import os
 
 from main_env import CinchMainEnv
 from get_agent import get_agent
-from obs_utils import build_mlp_obs
+from obs_utils_torch import build_mlp_obs
 
-MAIN_NET_CHECKPOINT_PATH = os.path.join("main_net", "checkpoints_large_batches_2", "ckpt_5800.pt")
+MAIN_NET_CHECKPOINT_PATH = os.path.join("checkpoints_large_batches_2", "ckpt_5800.pt")
 DEVICE = "cpu"
 
 env = CinchMainEnv()
@@ -17,7 +17,7 @@ x = build_mlp_obs(obs).to(DEVICE)  # Add batch dimension and move to device
 # 3. Export to an ONNX file
 torch.onnx.export(
     model, 
-    (x, legal),
+    (x),
     "main_net.onnx",
     input_names=["observation", "legal_actions"],
     output_names=["action_logits", "state_value"],